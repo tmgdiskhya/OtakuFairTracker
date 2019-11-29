66c033b911053ce27b757410f010be2f0e6c6f66
dikshuop98 <shiningzoro@gmail.com>
2019-11-29 09:12:33 +0545

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <item name="android.service.notification.Condition state">
+    <annotation name="android.support.annotation.IntDef">
+      <val name="value" val="{android.service.notification.Condition.STATE_FALSE, android.service.notification.Condition.STATE_TRUE, android.service.notification.Condition.STATE_UNKNOWN, android.service.notification.Condition.STATE_ERROR}" />
+    </annotation>
+  </item>
+  <item name="android.service.notification.NotificationListenerService java.util.List&lt;android.app.NotificationChannel&gt; getNotificationChannels(java.lang.String, android.os.UserHandle) 0">
+    <annotation name="android.support.annotation.NonNull" />
+  </item>
+  <item name="android.service.notification.NotificationListenerService java.util.List&lt;android.app.NotificationChannel&gt; getNotificationChannels(java.lang.String, android.os.UserHandle) 1">
+    <annotation name="android.support.annotation.NonNull" />
+  </item>
+  <item name="android.service.notification.NotificationListenerService java.util.List&lt;android.app.NotificationChannelGroup&gt; getNotificationChannelGroups(java.lang.String, android.os.UserHandle) 0">
+    <annotation name="android.support.annotation.NonNull" />
+  </item>
+  <item name="android.service.notification.NotificationListenerService java.util.List&lt;android.app.NotificationChannelGroup&gt; getNotificationChannelGroups(java.lang.String, android.os.UserHandle) 1">
+    <annotation name="android.support.annotation.NonNull" />
+  </item>
+  <item name="android.service.notification.NotificationListenerService void onNotificationChannelGroupModified(java.lang.String, android.os.UserHandle, android.app.NotificationChannelGroup, int) 3">
+    <annotation name="android.support.annotation.IntDef">
+      <val name="value" val="{android.service.notification.NotificationListenerService.NOTIFICATION_CHANNEL_OR_GROUP_ADDED, android.service.notification.NotificationListenerService.NOTIFICATION_CHANNEL_OR_GROUP_UPDATED, android.service.notification.NotificationListenerService.NOTIFICATION_CHANNEL_OR_GROUP_DELETED}" />
+    </annotation>
+  </item>
+  <item name="android.service.notification.NotificationListenerService void onNotificationChannelModified(java.lang.String, android.os.User
